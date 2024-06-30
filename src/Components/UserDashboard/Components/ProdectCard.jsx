7c2c91b1e0888cc6d3c789134a81aff9e5e4a708
mohan-ganta <gantamohan.556@gmail.com>
2024-06-30 18:00:04 +0530

    product card

---
+++ src/Components/UserDashboard/Components/ProdectCard.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function ProdectCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.andolasoft.com/images/new/famous-app/redbus-app.webp"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Bus Clone
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          ₹499
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
