d916ccdbcd3afc3f4e66b6da2f068ca428082f8e
Koteshwr-rao <koteswararaopakeeri@gmail.com>
2024-06-30 18:09:40 +0530

    admin

--- src/Components/AdminDashboard/Components/AddProducts/Addproducts.jsx
+++ src/Components/AdminDashboard/Components/AddProducts/Addproducts.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Button, Form, Input, Select, Upload } from "antd";
-import { UploadOutlined } from '@ant-design/icons';
+import { UploadOutlined } from "@ant-design/icons";
 import "./Addproducts.css";
 
 const Addproducts = () => {
+  const [imagefile, setImageFile] = useState();
+  const [file, setFile] = useState();
   const options = ["Books", "SourceCodes", "Arts & Drawings"];
   const handleSubmitForm = (vals) => {
     console.log(vals);
@@ -14,37 +16,20 @@ const Addproducts = () => {
   const onSearch = (value) => {
     console.log("search:", value);
   };
-  const [fileList, setFileList] = useState([
-    {
-      uid: '-1',
-      name: 'xxx.png',
-      status: 'done',
-      url: 'http://www.baidu.com/xxx.png',
-    },
-  ]);
-  const handleChange = (info) => {
-    let newFileList = [...info.fileList];
-
-    // 1. Limit the number of uploaded files
-    // Only to show two recent uploaded files, and old ones will be replaced by the new
-    newFileList = newFileList.slice(-2);
-
-    // 2. Read from response and show file link
-    newFileList = newFileList.map((file) => {
-      if (file.response) {
-        // Component will show file.url as link
-        file.url = file.response.url;
-      }
-      return file;
-    });
-    setFileList(newFileList);
-  };
-  const props = {
-    action: '..',
-    onChange: handleChange,
-    multiple: true,
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
   };
 
+  const handleImageChange = (e) => {
+    setImageFile(e.target.files[0]);
+  };
+  const handleFileUpload = () => {
+    console.log(file);
+  };
+
+  const handleImageUpload = () => {
+    console.log(imagefile);
+  };
   return (
     <div className="upload-single-product">
       <h1>Add Products</h1>
@@ -86,15 +71,15 @@ const Addproducts = () => {
         <Form.Item name="cost">
           <Input block placeholder="Enter your email address" required></Input>
         </Form.Item>
-        <Form.Item className="label" label="Product File"></Form.Item>
-        <Form.Item name="files">
-          <Input block placeholder="Enter your email address" required></Input>
-        </Form.Item>
         <Form.Item className="label" label="Product Image"></Form.Item>
+        <Form.Item name="files">
+          <Input type="file" onChange={handleImageChange}></Input>
+          <button onClick={handleFileUpload}>Upload</button>
+        </Form.Item>
+        <Form.Item className="label" label="Product File"></Form.Item>
         <Form.Item name="image">
-        <Upload {...props} fileList={fileList}>
-      <Button icon={<UploadOutlined />}>Upload</Button>
-    </Upload>
+          <Input type="file" onChange={handleFileChange} accept="*/image"></Input>
+          <button onClick={handleImageUpload}>Upload</button>
         </Form.Item>
         <Form.Item>
           <Button block type="primary" htmlType="submit">
